c60db72ce6708fd773e31f171a8f9dcf43bed653
smallslime <smallslime@gmail.com>
2016-09-26 15:19:26 +0800

    update

--- class/Config/JsonConfAdaptor.php
+++ class/Config/JsonConfAdaptor.php
@@ -5,13 +5,11 @@ class JsonConfAdaptor extends PHPConfAdaptor
 {
     public function load()
     {
-        $aData = $this->bDefault ?
+        $this->aData = $this->bDefault ?
             json_decode(file_get_contents($this->sCurrentFile), true) :
             array_merge(
                 json_decode(file_get_contents($this->sCurrentFile), true),
                 json_decode(file_get_contents($this->sDefaultFile), true)
             );
-
-        $this->aData = json_decode($aData, true);
     }
 }
